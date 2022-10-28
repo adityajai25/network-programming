f8fe0ee0230810a82b126dea48e8fdbf4f05f34e
adityajai25 <adityajai243@gmail.com>
2022-10-28 23:36:52 +0530

    first server file had been coded

---
+++ Python/simpleserver1.py
@@ -0,0 +1,11 @@
+import socket
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host,port))
+s.listen(6)
+while(True):
+    c,addr = s.accept()
+    print("Got acceptance from ",addr," successfully")
+    c.send("Message received thanks for connecting")
+    c.close()
