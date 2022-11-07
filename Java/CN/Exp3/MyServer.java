e37a9d89ff1da1497f6f045aea9155eaea17fc88
adityajai25 <adityajai243@gmail.com>
2022-11-07 23:55:54 +0530

    Exp3 completed

--- Java/CN/Exp3/MyServer.java
+++ Java/CN/Exp3/MyServer.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-public class MyServer {
+public class MyServer{
   public static void main(String[] args) {
     try {
       ServerSocket ss = new ServerSocket(6666);
