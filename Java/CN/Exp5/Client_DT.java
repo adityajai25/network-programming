7879c8ae527b999e956525bee91302f95a49c4c0
adityajai25 <adityajai243@gmail.com>
2022-11-22 09:50:26 +0530

    Done

--- Java/CN/Exp5/Client_DT.java
+++ Java/CN/Exp5/Client_DT.java
@@ -4,8 +4,7 @@ public class Client_DT {
   public static void main(String[] args) throws UnknownHostException, IOException {
     Socket cs = new Socket("LocalHost", 5000);
     System.out.println("Client " + cs.getInetAddress() + " is communicating from port No.: " +cs.getPort());
-    BufferedReader fromserver = new BufferedReader(new
-    InputStreamReader(cs.getInputStream()));
+    BufferedReader fromserver = new BufferedReader(new InputStreamReader(cs.getInputStream()));
     System.out.println(fromserver.readLine());
     fromserver.close();
     cs.close();
